a09ef7ac28076c828af657ce9139dc6ba4612894
eddie4510 <50067391+eddie4510@users.noreply.github.com>
2020-10-06 00:03:39 +0800

    use <App/> again

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-/*
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-*/
 
-ReactDOM.render(
-  <h1>Hello, My React page!</h1>,
-  document.getElementById('root')
-);
+
+
 //npm run build
 
 // If you want your app to work offline and load faster, you can change
