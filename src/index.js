196a77deed427d8c13a63bbca2003b392c9944ba
eddie4510 <50067391+eddie4510@users.noreply.github.com>
2020-10-05 23:35:45 +0800

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+
+ReactDOM.render(
+  <h1>Hello, React!</h1>,
   document.getElementById('root')
 );
 
