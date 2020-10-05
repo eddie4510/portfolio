35114f31e7c18c4af681d958b2b0002f32c48ba4
eddie4510 <50067391+eddie4510@users.noreply.github.com>
2020-10-06 00:07:24 +0800

    render again

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+*/
 
+ReactDOM.render(
+  <h1>Hi, my playground</h1>,
+  document.getElementById('root')
+);
 
 
 //npm run build
-
+//npm run deploy
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
