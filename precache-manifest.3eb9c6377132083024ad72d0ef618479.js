807867ed9818e8b94f65a7af5e624ec3f385c2be
eddie4510 <50067391+eddie4510@users.noreply.github.com>
2020-10-05 23:37:09 +0800

    Updates

--- precache-manifest.3eb9c6377132083024ad72d0ef618479.js
+++ precache-manifest.3eb9c6377132083024ad72d0ef618479.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "a8690b5cc3dfc264fe9345176bfa486e",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "687e579c0cdf732c4b94",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "687e579c0cdf732c4b94",
+    "url": "/portfolio/static/js/main.1b3e0ca4.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0da11d69fbca1a6be6b699b2717e1562.js"
+  "/portfolio/precache-manifest.3eb9c6377132083024ad72d0ef618479.js"
 );
 
 self.addEventListener('message', (event) => {
