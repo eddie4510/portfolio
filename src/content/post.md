22a53b9f22e7d7f56475cac9648aba0942771610
eddie4510 <50067391+eddie4510@users.noreply.github.com>
2020-10-07 18:57:06 +0800

    md file handling done

--- src/content/post.md
+++ src/content/post.md
@@ -1,9 +1,9 @@
----
-title: My First Post
-author: Eddie
-date: 2020/10/06
----
+
 
 # Welcome to my playground
 
-**It's a beautiful thing.*
+It's a beautiful thing.
+
+- hello
+- no
+- yeah
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactMarkdown from 'react-markdown';
 import './index.css';
+import './sideBar.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import post1 from "./content/post.md";
+import post2 from "./content/post2.md";
 /*
 ReactDOM.render(
   <React.StrictMode>
@@ -12,27 +16,67 @@ ReactDOM.render(
 );
 */
 
+
 function NavBar() {
 
+  return <nav className="navbar">
+    <ul className="navbar-nav">
+      <li>
+        <a href="/" className="logo">
+          <span>PORTFOLIO</span>
+          <i id="logo" className="fas fa-angle-double-right"></i>
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="web" className="nav-link">
+          <i className="fas fa-globe"></i>
+          <span className="link-text">WEB</span>
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="game" className="nav-link">
+          <i className="fas fa-gamepad"></i>
+          <span className="link-text">GAME</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 }
 
-function Main() {
-  return <div>
-    <h1>Hi, my playground</h1>
-    <p>
-      I'm baby kale chips affogato ennui lumbersexual, williamsburg paleo quinoa
-      iceland normcore tumeric. Kitsch coloring book retro, seitan schlitz
-      tattooed biodiesel vexillologist neutra. Synth mumblecore deep v, umami
-      selfies normcore gluten-free snackwave. Seitan ramps drinking vinegar
-      venmo keytar, humblebrag VHS post-ironic tacos godard pour-over.
-    </p>
-  </div>
+//implement endsWith function
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function (search, this_len) {
+    if (this_len === undefined || this_len > this.length) {
+      this_len = this.length;
+    }
+    return this.substring(this_len - search.length, this_len) === search;
+  };
 }
 
-const el = <Main />;
+var mainEl = '';
+function checkUrl() {
+  let url = window.location+"";
+  if (url.endsWith("game")) {
+
+    mainEl = <ReactMarkdown source={post1} />;
+  }
+  else if (url.endsWith("web")) {
+    mainEl = <ReactMarkdown source={post2} />;
+  }
+}
+
+checkUrl();
+
+
+let navEl = <NavBar />;
+ReactDOM.render(
+  navEl,
+  document.getElementById('nav')
+);
+
 
 ReactDOM.render(
-  el,
+  mainEl,
   document.getElementById('main')
 );
 
